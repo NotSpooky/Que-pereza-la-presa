d7d9adeeceb8720a38d4cb75d945cdda6399b0f3
andreagomez <andreittttta@hotmail.com>
2017-07-18 22:09:14 +0000

    about

---
+++ src/Controller/AboutController.php
@@ -0,0 +1,111 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * About Controller
+ *
+ * @property \App\Model\Table\AboutTable $About
+ *
+ * @method \App\Model\Entity\About[] paginate($object = null, array $settings = [])
+ */
+class AboutController extends AppController
+{
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|void
+     */
+    public function index()
+    {
+        $about = $this->paginate($this->About);
+
+        $this->set(compact('about'));
+        $this->set('_serialize', ['about']);
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id About id.
+     * @return \Cake\Http\Response|void
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $about = $this->About->get($id, [
+            'contain' => []
+        ]);
+
+        $this->set('about', $about);
+        $this->set('_serialize', ['about']);
+    }
+
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $about = $this->About->newEntity();
+        if ($this->request->is('post')) {
+            $about = $this->About->patchEntity($about, $this->request->getData());
+            if ($this->About->save($about)) {
+                $this->Flash->success(__('The about has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The about could not be saved. Please, try again.'));
+        }
+        $this->set(compact('about'));
+        $this->set('_serialize', ['about']);
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id About id.
+     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     */
+    public function edit($id = null)
+    {
+        $about = $this->About->get($id, [
+            'contain' => []
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $about = $this->About->patchEntity($about, $this->request->getData());
+            if ($this->About->save($about)) {
+                $this->Flash->success(__('The about has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The about could not be saved. Please, try again.'));
+        }
+        $this->set(compact('about'));
+        $this->set('_serialize', ['about']);
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id About id.
+     * @return \Cake\Http\Response|null Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $about = $this->About->get($id);
+        if ($this->About->delete($about)) {
+            $this->Flash->success(__('The about has been deleted.'));
+        } else {
+            $this->Flash->error(__('The about could not be deleted. Please, try again.'));
+        }
+
+        return $this->redirect(['action' => 'index']);
+    }
+}
---
+++ tests/Fixture/AboutFixture.php
@@ -0,0 +1,56 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * AboutFixture
+ *
+ */
+class AboutFixture extends TestFixture
+{
+
+    /**
+     * Table name
+     *
+     * @var string
+     */
+    public $table = 'about';
+
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // @codingStandardsIgnoreStart
+    public $fields = [
+        'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+        'photo' => ['type' => 'string', 'length' => 255, 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'text' => ['type' => 'text', 'length' => null, 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null],
+        'created' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+        'modified' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+        ],
+        '_options' => [
+            'engine' => 'InnoDB',
+            'collation' => 'utf8_general_ci'
+        ],
+    ];
+    // @codingStandardsIgnoreEnd
+
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = [
+        [
+            'id' => 1,
+            'photo' => 'Lorem ipsum dolor sit amet',
+            'text' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+            'created' => '2017-07-18 22:04:17',
+            'modified' => '2017-07-18 22:04:17'
+        ],
+    ];
+}
